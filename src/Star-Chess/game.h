cd30d44fdd28b17a0958a859ebbb14eac8ac6868
RobertBu1 <111591413+RobertBu1@users.noreply.github.com>
2023-04-07 13:08:03 -0600

    Update game.h

--- src/Star-Chess/game.h
+++ src/Star-Chess/game.h
@@ -1,176 +1,183 @@
-//Luca DalCanto, Spencer Wood
+// Luca DalCanto, Spencer Wood
+//  libraries:
+#include <chrono>
+#include <fstream>
 #include <iostream>
-#include <stdlib.h>
+#include <list>
+#include <thread>
 
 using namespace std;
 
-class Pawn {
-  bool bottom, wsquare;
+ifstream WhitePawnText("white_pawn.txt");
+ifstream BlackPawnText("pawn_black.txt");
+
+class Piece {
+public:
+  enum pieceType { BLANK, PAWN, KING, QUEEN, KNIGHT, BISHOP, ROOK };
+  string output;
+  bool wsquare;
   bool startingPosition;
+  bool rebels;
+  pieceType piece;
+  int currentLine = 0;
+  string file;
 
-  Pawn() {}
-
-  void display() {
-    if (wsquare) {
-      if (bottom) {
-        cout << " ";
+  Piece(string myPiece = "PAWN", bool onWhite = true, bool isRebel = true) {
+    wsquare = onWhite;
+    rebels = isRebel;
+    if (myPiece == "BLANK"){
+      
+    }
+    if (myPiece == "PAWN") {
+      piece = PAWN;
+      if (rebels == true) {
+        file = "pawn_white.txt";
       } else {
-        cout << " ";
+       file = "pawn_black.txt"; 
       }
-    } else {
-      if (bottom) {
-        cout << " ";
-      } else {
-        cout << " ";
-      }
+    } else if (myPiece == "KING") {
+      piece = KING;
+    } else if (myPiece == "QUEEN") {
+      piece = QUEEN;
+    } else if (myPiece == "KNIGHT") {
+      piece = KNIGHT;
+    } else if (myPiece == "BISHOP") {
+      piece = BISHOP;
+    } else if (myPiece == "ROOK") {
+      piece = ROOK;
     }
   }
 
-  void move() { // write logic for how a pawn can move, figure out how to choose
-                // a piece
+  string display(int lineNum) {
+    if (!rebels) {
+      BlackPawnText.open("file");
+    if (wsquare) {
+      while (!BlackPawnText.eof()) { //Gets the line of a Black Pawn on a white square
+        currentLine++;
+        getline(BlackPawnText, output);
+        if (currentLine == lineNum) {
+          break;
+        }
+      } 
+      // turn "#" into " "
+      return output;
+      cout << " ";
+     } 
+    
+    else {
+      while (!BlackPawnText.eof()) { //Gets the line of Black Pawn on a black square
+        currentLine++;
+        getline(BlackPawnText, output);
+        if (currentLine == lineNum) {
+          break;
+        }
+      }
+      return output;
+    }
+    BlackPawnText.close();
+    } else {
+      //WhitePawnText.open("file");
+    if (wsquare) { //Gets the line of white pawn on a white square
+      while (!WhitePawnText.eof()) {
+        currentLine++;
+        getline(WhitePawnText, output);
+        if (currentLine == lineNum) {
+          break;
+        }
+      }
+      // turn "#" into " "
+      return output;
+      cout << " ";
+     } else {
+      cout << "Running this stuff:";
+      string tempOut;
+      currentLine = 0;
+      while (getline(WhitePawnText, tempOut)){
+        currentLine ++;
+        output = tempOut;
+        if (currentLine == lineNum) {
+          break;
+        }
+      }
+      // while (!WhitePawnText.eof()) { //Gets the line of a white pawn on a black square
+      //   currentLine++;
+      //   getline(WhitePawnText, output);
+      //   if (currentLine == lineNum) {
+      //     break;
+      //   }
+      // }
+      cout << output; //<-- for debug purposes
+      return output;
+    }
+    WhitePawnText.close();
+    }
+  }
+
+  void
+  getMoves() { // write logic for how a pawn can move, figure out how to choose
+               // a piece
     if (startingPosition) {
-      if (bottom) {
-
-      } else {
-      }
-    } else {
-      if (bottom) {
-
-      } else {
-      }
-    }
-  }
-};
-
-class King {
-  bool bottom, wsquare;
-
-  King() {}
-
-  void display() {
-    if (wsquare) {
-      if (bottom) {
-        cout << " ";
-      } else {
-        cout << " ";
-      }
-    } else {
-      if (bottom) {
-        cout << " ";
-      } else {
-        cout << " ";
-      }
-    }
-  }
-
-  void move() {
-    if (bottom) {
 
     } else {
     }
   }
 };
 
-// Do after Pawn and King are figured out
-//  class Knight{};
-//  class Bishop{};
-//  class Rook{};
-//  class Queen{};
-
-//----------------------------------------------
+//---------------------------------------------------------------------------------------------
 
 class RegularGame {
 public:
   RegularGame(int playerCount) {
     players = playerCount;
     if ((rand() % 1) == 1 || players == 1) { // white is bottom
-      string tempArray[64] = {"br",
-                              "bn",
-                              "bb",
-                              "bq",
-                              "bk",
-                              "bb",
-                              "bn",
-                              "br"
-                              "bp",
-                              "bp",
-                              "bp",
-                              "bp",
-                              "bp",
-                              "bp",
-                              "bp",
-                              "bp",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "no",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wp",
-                              "wr",
-                              "wn",
-                              "wb",
-                              "wq",
-                              "wk",
-                              "bn",
-                              "wn",
-                              "wr"};
-      storeMemory(tempArray);
+      string tempArray[64] = {"ROOK", "KNIGHT", "BISHOP", "QUEEN", "KING","BISHOP","KNIGHT","ROOK",
+                              "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN",
+                              "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK","BLANK","BLANK",
+                              "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK","BLANK","BLANK",
+                              "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK","BLANK","BLANK",
+                              "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK","BLANK","BLANK",
+                              "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN",
+                              "ROOK", "KNIGHT", "BISHOP", "QUEEN","KING","BISHOP","KNIGHT","ROOK"};
+      for(int i = 0; i < 64; i++){
+        Piece tempPiece(tempArray[i], (int(i % 2) == (i % 2)), (i > 32));
+        grid[i] = tempPiece;
+      }
     } else { // black is bottom
       string tempArray[64] = {
-          "wr", "wn", "wb", "wq", "wk", "bn", "wn", "wr", "wp", "wp", "wp",
-          "wp", "wp", "wp", "wp", "wp", "no", "no", "no", "no", "no", "no",
-          "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no",
-          "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no",
-          "no", "no", "no", "no", "bp", "bp", "bp", "bp", "bp", "bp", "bp",
-          "bp", "br", "bn", "bb", "bq", "bk", "bb", "bn", "br",
-      };
-      storeMemory(tempArray);
+          "ROOK", "KNIGHT", "BISHOP", "KING", "QUEEN", "BISHOP", "KNIGHT", "ROOK",
+          "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN",
+          "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK",
+          "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK",
+          "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK",
+          "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK", "BLANK",
+          "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN", "PAWN",
+          "ROOK", "KNIGHT", "BISHOP", "KING", "QUEEN", "BISHOP", "KNIGHT", "ROOK"};
+      for(int i = 0; i < 64; i++){
+        Piece tempPiece(tempArray[i], (int(i % 2) == (i % 2)), (i < 32));
+        grid[i] = tempPiece;
+      }
+    }
+  }
+
+  void displayBoard() {
+    for (int i = 1; i < 5; i++) {
+      string thisLine = "";
+      for(int i = 8; i < 16; i++){
+        thisLine += grid[i].display(i); 
+      }
+      cout << thisLine + "\n";
     }
   }
 
 private:
+  Piece grid[64];
   int players;
-  string grid[64];
 
-  void storeMemory(string tempGrid[64]) {
-    for (int i = 0; i < 64; i++) {
-      grid[i] = tempGrid[i];
-    }
-  }
+  // void storeMemory(string tempGrid[64]) {
+  //   for (int i = 0; i < 64; i++) {
+  //     grid[i] = tempGrid[i];
+  //   }
+  // }
 };
 
 //-----------------------------------------------
